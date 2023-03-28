61b17de1a93a6ced6f0b6b8133f8ddb291e27554
99yuseong <dbtjd6511@gmail.com>
2023-03-28 12:58:32 +0900

    add sugar api

--- rxswift-practice/rxswift-practice/ViewController.swift
+++ rxswift-practice/rxswift-practice/ViewController.swift
@@ -122,6 +122,22 @@ extension ViewController {
     // 5. Disposed
     
     private func downloadJson(_ url: String) -> Observable<String?> {
+        
+        // Syntatic Sugar
+        
+        // 0. Base
+        //        return Observable.create { emitter in
+        //            emitter.onNext("Hello world")
+        //            emitter.onCompleted()
+        //            return Disposables.create ()
+        //        }
+        
+        // 1. Observable.just() 1개 전달할 때
+//        return Observable.just("Hello world")
+        
+        // 2. Observable.from([]) 여러 번으로 나눠서 전달할 때
+//        return Observable.from(["Hello", "world"])
+        
         // 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
         return Observable.create { emitter in
             let url = URL(string: url)!
@@ -130,20 +146,20 @@ extension ViewController {
                     emitter.onError(err!)
                     return
                 }
-                
+
                 if let data = data, let json = String(data: data, encoding: .utf8) {
                     emitter.onNext(json)
                 }
-                
+
                 emitter.onCompleted()
             }
             task.resume()
-            
+
             return Disposables.create() {
                 // 중간 종료 시 cancel 작업
                 task.cancel()
             }
-            
+
         }
         
 //        return Observable.create() { f in
@@ -167,20 +183,33 @@ extension ViewController {
         
         // 2. Observable로 오는 데이터를 받아서 처리하는 방법
         let disposable = downloadJson(MEMBER_LIST_URL)
-            .debug()
-            .subscribe { event in
-            switch event {
-            case .next(let json):
-                DispatchQueue.main.async {
+            .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .default)) // operator: subscribe 시작할 때 어느 쓰레드에서 진행할 것인 지
+            .map({ json in json?.count ?? 0 }) // operator: json 존재하는 지 확인
+            .filter({ cnt in cnt > 0 }) // operator: count > 0인 것만 필터링
+            .map({ "\($0)" }) // operator: Int -> String 변환
+            .observe(on: MainScheduler.instance) // operator: 앞으로 나오는 것들은 메인쓰레드에서 진행
+            .subscribe(
+                onNext: { json in
                     self.editText.text = json
                     self.setVisibleWithAnimation(self.indicator, false)
-                }
-            case .completed:
-                break
-            case .error(let err):
-                break
-            }
-        }
+                    print(json as Any)
+                },
+                onCompleted: { print("Com") },
+                onDisposed: { print("dispose") })
+//            .subscribe { event in
+//            switch event {
+//            case .next(let json):
+//                DispatchQueue.main.async {
+//                    self.editText.text = json
+//                    self.setVisibleWithAnimation(self.indicator, false)
+//                }
+//                print(json)
+//            case .completed:
+//                break
+//            case .error(let err):
+//                break
+//            }
+//        }
         
 //        disposable.dispose() // 필요에 의해 호출 가능, 중간에 호출 시 바로 observable이 dispose되어 동작이 이뤄지지 않음
     }
